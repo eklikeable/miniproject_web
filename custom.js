ec177afbefaf4beb4106b2eea91f16b024263a14
EK Kim <lllllleaders@gmail.com>
2022-04-27 12:55:05 +0900

    create accordion

---
+++ custom.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.accordion-item .title').click(function(){
+    $(this).next().stop().slideToggle(300)
+    $(this).toggleClass('active')
+  })
+  $('.accordion-item .content').click(function(){
+    $(this).stop().slideUp()
+  })
+})
